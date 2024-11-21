2e74555226321261c4ce9bc8e1e74dfc8581618c
Haris Tajammal <haris8306827@gmail.com>
2024-11-21 12:12:37 +0500

    Add files via upload

---
+++ Check number is in range.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter a number:"<<endl;
+	cin>>a;
+	if(a>10&&a<=20)
+	cout<<"The number is in range";
+	else
+	cout<<"The number is not in range";
+	return 0;
+}
